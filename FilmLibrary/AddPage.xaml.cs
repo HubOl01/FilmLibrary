cbed66eee4aaef756daba4bd17f3afb91c551152
Oleg Kulichkov <kuloleg52@gmail.com>
2025-08-28 15:20:37 +0300

    Добавлены разделы списка, добавления и валидации текстовых полей

---
+++ FilmLibrary/AddPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FilmLibrary
+{
+    public partial class AddPage : UserControl
+    {
+
+        public AddPage()
+        {
+            InitializeComponent();
+            nameTextBox.TextChanged += ValidateFields;
+            descTextBox.TextChanged += ValidateFields;
+            imageTextBox.TextChanged += ValidateFields;
+            yearReleaseTextBox.TextChanged += ValidateFields;
+            rateTextBox.TextChanged += ValidateFields;
+            CategoryComboBox.SelectionChanged += ValidateFields;
+
+            // Первоначальная проверка состояния полей
+            ValidateFields(null, null);
+        }
+
+        private void ImageText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (Uri.TryCreate(textBox?.Text, UriKind.Absolute, out var uri) &&
+                (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps))
+            {
+                image.Source = new System.Windows.Media.Imaging.BitmapImage(uri);
+            }
+            else
+            {
+                image.Source = null; 
+            }
+            ValidateFields(sender, e);
+        }
+        private void ValidateFields(object sender, EventArgs e)
+        {
+            // Проверяем, что все обязательные поля заполнены
+            bool allFieldsFilled =
+                !string.IsNullOrWhiteSpace(nameTextBox.Text) &&
+                !string.IsNullOrWhiteSpace(descTextBox.Text) &&
+                !string.IsNullOrWhiteSpace(imageTextBox.Text) &&
+                !string.IsNullOrWhiteSpace(yearReleaseTextBox.Text) &&
+                !string.IsNullOrWhiteSpace(rateTextBox.Text) &&
+                !string.IsNullOrWhiteSpace(CategoryComboBox.Text);
+
+            // Обновляем состояние кнопки
+            button1.IsEnabled = allFieldsFilled;
+        }
+
+        private void addFilm_Click(object sender, RoutedEventArgs e)
+        {
+            Trace.WriteLine("Clicked!!!");
+            if(string.IsNullOrEmpty(nameTextBox.Text) &&
+                string.IsNullOrEmpty(descTextBox.Text) &&
+                string.IsNullOrEmpty(imageTextBox.Text) &&
+                string.IsNullOrEmpty(yearReleaseTextBox.Text) &&
+                string.IsNullOrEmpty(rateTextBox.Text) &&
+                string.IsNullOrEmpty(CategoryComboBox.Text)
+                )
+            {
+
+                Trace.WriteLine($"{nameTextBox.Text} - {descTextBox.Text} - {imageTextBox.Text} - {CategoryComboBox.Text}");
+            }
+        }
+    }
+}
--- FilmLibrary/MainWindow.xaml.cs
+++ FilmLibrary/MainWindow.xaml.cs
@@ -17,6 +17,36 @@ namespace FilmLibrary
         {
             InitializeComponent();
         }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid();
+            gridPages.Children.Add(new AddPage());
+        }
+        private void List_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid();
+            gridPages.Children.Add(new ListPage());
+        }
+        private void Actors_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid();
+            gridPages.Children.Add(new ActorsPage());
+        }
+       
+        private void ClearGrid()
+        {
+            gridPages.Children.Clear();
+        }
+
+        private void AddPage_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
 
